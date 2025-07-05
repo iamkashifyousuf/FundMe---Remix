3d6b21c5b8086e172cec54239736f54c73129de0
iamkashifyousuf <kashifyousuf.sc@gmail.com>
2025-07-05 13:49:08 +0500

    updated FundMe.sol

--- FundMe.sol
+++ FundMe.sol
@@ -1,51 +1,57 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.18;
+//SPDX-License-Identifier: MIT
 
-// import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
-// library {}
-// import {PriceConverter} from "./PriceConverter.sol"
+pragma solidity 0.8.24;
 
-contract Fund {
+import {PriceConverter} from "./PriceConverter.sol";
 
-    // using PriceConverter for uint256;
-    address public immutable i_owner;
-    uint256 public constant MINIMUMS_ETH = 1e18;
+error Notowner();
+
+contract FundMe {
+
+    using PriceConverter for uint256;
+    uint256 public constant MINIMUM_USD = 5 * 1e18;
     address[] public funders;
     mapping(address funder => uint256 amountFunded) public funderToAmountFunded;
-
-
+    address public immutable i_owner;
     constructor() {
         i_owner = msg.sender;
     }
 
     function fundMe() public payable {
-        require(msg.value >= MINIMUMS_ETH, "Not get enought Eth");
+        require(msg.value.getConversion() >= MINIMUM_USD, "Don't Get enough Ethers i.e. > 5 etheeeeeeeers");
         funders.push(msg.sender);
-        funderToAmountFunded[msg.sender] += msg.value;
+        funderToAmountFunded[msg.sender] = funderToAmountFunded[msg.sender] + msg.value;
     }
 
-    // function getPrice() public view returns(uint256){
-    //     AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-    //     (,int256 answer,,,) = priceFeed.latestRoundData();
-    //     return uint256(answer*1e10);
-    // }
-
-    // function getConversion(uint256 ethFunded) public view returns(uint256) {
-    //     uint256 ethPrice = getPrice();
-    //     uint256 ethAmount = (ethPrice * ethFunded) / 1e18;
-    //     return ethAmount;
-    // }
-
-    function withdraw() public {
-        require(msg.sender == i_owner, "Only Owner do witdrawal");
-        (bool callSuccees,) = payable(msg.sender).call{value: address(this).balance}("");
-        require(callSuccees,"Call failed");
-        for(uint256 index = 0; index < funders.length; index++) {
-            address funder = funders[index];
-            funderToAmountFunded[funder] = 0; 
-        }
-        funders = new address[](0);
+    function withdraw() public onlyOwners {
+    for(uint256 funderindex = 0; funderindex < funders.length; funderindex++) {
+        address funder = funders[funderindex];
+        funderToAmountFunded[funder] = 0;
     }
+    funders = new address[](0);
+    // //transfer
+    // payable(msg.sender).transfer(address(this).balance);                                  // If txn failed it throgh an error. Auto reverted.
+    // //send
+    // bool sendSuccess = payable(msg.sender).send(address(this).balance);                   // It return bool that explain txn stuatus. Noauto reverted
+    // require(sendSuccess, "Send Failed");
+    //call
+    (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");    // 
+    require(callSuccess, "Call Failed");
+    }
+
+    modifier onlyOwners() {
+        // require(msg.sender == i_owner, Notowner());
+        if(msg.sender == i_owner) { revert Notowner();}
+        _;
+    }
+
+    receive() external payable {
+        fundMe();
+    }
+
+    fallback() external payable {
+        fundMe();
+     }
 }
 
 
